4eeb0366c7d85c6e410dc408c4c811834029b23b
Bruno Pontiz <bpontiz@gmail.com>
2022-09-01 21:09:30 -0300

    Deleting NavBar to fix vercel build error.

--- src/Components/Navbar/CartWidget.jsx
+++
@@ -1,9 +0,0 @@
-import { BsFillCartFill } from 'react-icons/bs';
-
-const CartWidget = () => {
-    return (
-        <BsFillCartFill style={{minWidth: "25px"}} />
-    )
-}
-
-export default CartWidget;
